b02929c2aaa91db5c159e9292e011381a57addce
salviega <salviega6@gmail.com>
2025-04-23 15:06:59 -0500

    🚚 Moved constants and models folder to ./

---
+++ src/models/index.ts
@@ -0,0 +1,14 @@
+export interface Category {
+	title: string
+	href: string
+	label: string
+	img: string
+}
+
+export interface Professional {
+	id: number
+	name: string
+	city: string
+	stars: number
+	description: string
+}
