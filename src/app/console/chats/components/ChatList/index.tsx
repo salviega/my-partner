ed375b7457bbcccafba0d69477dbc006df51e966
salviega <salviega6@gmail.com>
2025-05-03 11:43:45 -0500

    🐛 Fixed props of article section

--- src/app/console/chats/components/ChatList/index.tsx
+++ src/app/console/chats/components/ChatList/index.tsx
@@ -28,7 +28,7 @@ const ChatListComponent: React.FC<ChatListProps> = ({
 			return
 		}
 
-		socket = io('http://localhost:3000', {
+		socket = io('http://186.113.88.214:3000', {
 			query: { userID: currentUserId }
 		})
 
